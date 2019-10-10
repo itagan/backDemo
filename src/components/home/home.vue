3892b1547e7b3108cb5e2614494c17c8a562503d
System Administrator <root@agandeMacBook-Pro.local>
2019-10-10 11:45:13 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,20 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+          <img src="../../assets/img/my.png" alt="无法显示图片" class="myimg">
+        </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+            <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="2"><div class="grid-content bg-purple">
+          <a href="#" class="loginout">退出</a>
+        </div></el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="aside">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -22,6 +36,10 @@
   .header {
     background-color: #b3c0d1;
   }
+  .header .myimg {
+    height: 60px;
+    width: 80px;
+  }
 
   .aside {
     background-color: #d3dce6;
@@ -31,4 +49,14 @@
     background-color: #e9eef3;
     height: 100%;
   }
+
+  .middle {
+    text-align: center;
+    color: red;
+  }
+  .loginout {
+    line-height: 60px;
+    text-decoration: none;
+  }
+
 </style>
