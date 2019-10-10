efeb127553b6666a23b79b36bc8688608ad07c79
agan <agan@agandeMacBook-Pro.local>
2019-10-10 17:48:48 +0800

    列表面包屑搜索框表格数据获取

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const MyHttpServer = {}
 MyHttpServer.install = (Vue) => {
   axios.defaults.baseURL = 'http://39.108.193.251:8888/api/private/v1/'
+  // axios.defaults.baseURL = 'http://api.xiaomadagege.cn:8888/api/private/v1/'
 
   // 4. 添加实例方法
   Vue.prototype.$http = axios
