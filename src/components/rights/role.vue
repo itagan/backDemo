e022bfa0bafcd6a431dbfa89f7fed4e72debdd5e
System Administrator <root@agandeMacBook-Pro.local>
2019-10-12 10:45:34 +0800

    删除角色权限的优化

--- src/components/rights/role.vue
+++ src/components/rights/role.vue
@@ -20,19 +20,19 @@
         <template slot-scope="scope">
           <el-row v-for="(item1,i) in scope.row.children" :key="i">
             <el-col :span="4">
-              <el-tag @close="deleRight(scope.row.id,item1.id)" closable>{{item1.authName}}</el-tag>
+              <el-tag @close="deleRight(scope.row,item1.id)" closable>{{item1.authName}}</el-tag>
               <i class="el-icon-arrow-right"></i>
             </el-col>
 
             <el-col :span="20">
               <el-row v-for="(item2,i) in item1.children" :key="i">
                 <el-col :span="4">
-                  <el-tag @close="deleRight(scope.row.id,item2.id)" type="success" closable >{{item2.authName}}</el-tag>
+                  <el-tag @close="deleRight(scope.row,item2.id)" type="success" closable >{{item2.authName}}</el-tag>
                   <i class="el-icon-arrow-right"></i>
                 </el-col>
 
                 <el-col :span="20">
-                  <el-tag v-for="(item3,i) in item2.children" :key="i" type="warning" closable @close="deleRight(scope.row.id,item3.id)"
+                  <el-tag v-for="(item3,i) in item2.children" :key="i" type="warning" closable @close="deleRight(scope.row,item3.id)"
                   >{{item3.authName}}</el-tag>
                 </el-col>
 
@@ -108,11 +108,14 @@
     },
     methods: {
       //取消权限
-      async deleRight(roleId,rightId) {
-        const res = await this.$http.delete(`roles/${roleId}/rights/${rightId}`)
+      async deleRight(role,rightId) {
+        const res = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
         // console.log(res)
-        this.getRolelist()
-
+        //roleId 当前角色id  ightId当前权限id
+        //删除成功 返回了状态200和该角色的剩余权限
+        // this.getRolelist()
+        //优化，删除不应该更新整个视图，而是根据返回的数据更新当前角色的权限，达到删除可以看到删除效果，而不被删除的不会被刷新 所以传入是角色 为参数
+        role.children = res.data.data
       },
       async getRolelist() {
         const res = await this.$http.get(`roles`)
