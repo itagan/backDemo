7d24a833b081861b335d6c4cfc6fa034b2b115ca
System Administrator <root@agandeMacBook-Pro.local>
2019-10-26 10:18:37 +0800

    登录提示

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -6,10 +6,10 @@
       :model="formdata">
       <h2>用户登录</h2>
       <el-form-item label="用户名">
-        <el-input v-model="formdata.username"></el-input>
+        <el-input v-model="formdata.username" placeholder="admin"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formdata.password"></el-input>
+        <el-input v-model="formdata.password" placeholder="123456"></el-input>
       </el-form-item>
       <el-button
         @click.prevent="handleLogin"
